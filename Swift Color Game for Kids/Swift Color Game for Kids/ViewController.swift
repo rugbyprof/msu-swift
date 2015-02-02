a8d1c1d5ca8c7ef78aaac8701edfb24b289ea0ed
rugbyprof <terry.griffin@mwsu.edu>
2015-02-02 15:43:00 -0600

    added color matching game

---
+++ Swift Color Game for Kids/Swift Color Game for Kids/ViewController.swift
@@ -0,0 +1,196 @@
+//
+//  ViewController.swift
+//  Swift Color Game for Kids
+//
+//  Created by Terry Griffin on 2/2/15.
+//  Copyright (c) 2015 mwsu. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var lblChangeColor: UILabel!
+    @IBOutlet weak var lblTotalCorrect: UILabel!
+    @IBOutlet weak var lblCorrectIncorrect: UILabel!
+    
+    @IBOutlet weak var btnAnswer1OUTLET: UIButton!
+    @IBOutlet weak var btnAnswer2OUTLET: UIButton!
+    @IBOutlet weak var btnAnswer3OUTLET: UIButton!
+    
+    
+    var buttonAnswer = 0
+    
+    var colorNumber = 0
+    
+    var button1Correct = false
+    var button2Correct = false
+    var button3Correct = false
+    
+    var totalCorrect = 0;
+    
+    var colorRGB : [Dictionary<String,UInt>] = [
+        ["Red":0xFF0000],
+        ["Green":0x00FF00],
+        ["Blue":0x0000FF],
+        ["Orange":0xFF7F00],
+        ["Black":0x000000],
+        ["White":0xFFFFFF],
+        ["Pink":0xFF6EB4],
+        ["Purple":0x8968CD],
+        ["Gray":0xBEBEBE],
+        ["Yellow":0xFFFF00]
+    ]
+    
+    var colorNames : [String] = ["Red","Green","Blue","Orange","Black","White","Brown","Purple","Gray","Yellow"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        matchColorLogic();
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnAnswer1ACTION(sender: UIButton) {
+        if button1Correct == true {
+            lblCorrectIncorrect.text = "Correct!"
+            totalCorrect++
+        }else{
+            lblCorrectIncorrect.text = "Incorrect!"
+        }
+        button1Correct = false
+        matchColorLogic()
+    }
+    
+    @IBAction func btnAnswer2ACTION(sender: UIButton) {
+        if button2Correct == true {
+            lblCorrectIncorrect.text = "Correct!"
+            totalCorrect++
+        }else{
+            lblCorrectIncorrect.text = "Incorrect!"
+        }
+        button2Correct = false
+        matchColorLogic()
+    }
+    
+    @IBAction func btnAnswer3ACTION(sender: UIButton) {
+        if button3Correct == true {
+            lblCorrectIncorrect.text = "Correct!"
+            totalCorrect++
+        }else{
+            lblCorrectIncorrect.text = "Incorrect!"
+        }
+        button3Correct = false
+        matchColorLogic()
+    }
+    
+    
+    func matchColorLogic() -> Bool{
+        buttonAnswer = Int(arc4random_uniform(2))
+        colorNumber = Int(arc4random_uniform(9))
+        
+        changeColor()
+        
+        var color = colorNumber
+        var prev = ((colorNumber - 1) + colorNames.count) % colorNames.count
+        var next = (colorNumber + 1) % colorNames.count
+        
+        if(buttonAnswer == 0){
+            btnAnswer1OUTLET.setTitle(colorNames[color], forState: UIControlState.Normal)
+            btnAnswer2OUTLET.setTitle(colorNames[prev], forState: UIControlState.Normal)
+            btnAnswer3OUTLET.setTitle(colorNames[next], forState: UIControlState.Normal)
+            button1Correct = true;
+            
+        }
+        
+        if(buttonAnswer == 1){
+            btnAnswer1OUTLET.setTitle(colorNames[prev], forState: UIControlState.Normal)
+            btnAnswer2OUTLET.setTitle(colorNames[color], forState: UIControlState.Normal)
+            btnAnswer3OUTLET.setTitle(colorNames[next], forState: UIControlState.Normal)
+            button2Correct = true
+        }
+        
+        if(buttonAnswer == 2){
+            btnAnswer1OUTLET.setTitle(colorNames[next], forState: UIControlState.Normal)
+            btnAnswer2OUTLET.setTitle(colorNames[prev], forState: UIControlState.Normal)
+            btnAnswer3OUTLET.setTitle(colorNames[color], forState: UIControlState.Normal)
+            button3Correct = true
+        }
+        
+        return true;
+    }
+
+    
+    func changeColor() -> Bool{
+
+        if(colorNumber == 0){
+            lblChangeColor.backgroundColor = UIColor.redColor()
+        }
+        if(colorNumber == 1){
+            lblChangeColor.backgroundColor = UIColor.greenColor()
+        }
+        if(colorNumber == 2){
+            lblChangeColor.backgroundColor = UIColor.blueColor()
+        }
+        if(colorNumber == 3){
+            lblChangeColor.backgroundColor = UIColor.orangeColor()
+        }
+        if(colorNumber == 4){
+            lblChangeColor.backgroundColor = UIColor.blackColor()
+        }
+        if(colorNumber == 5){
+            lblChangeColor.backgroundColor = UIColor.whiteColor()
+        }
+        if(colorNumber == 6){
+            lblChangeColor.backgroundColor = UIColor.brownColor()
+        }
+        if(colorNumber == 7){
+            lblChangeColor.backgroundColor = UIColor.purpleColor()
+        }
+        if(colorNumber == 8){
+            lblChangeColor.backgroundColor = UIColor.grayColor()
+        }
+        if(colorNumber == 9){
+            lblChangeColor.backgroundColor = UIColor.yellowColor()
+        }
+        
+        
+        return true
+    }
+    
+    
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
+
+class colorRGB {
+    var colorRGB : [Dictionary<String,UInt>] = [
+        ["Red":0xFF0000],
+        ["Green":0x00FF00],
+        ["Blue":0x0000FF],
+        ["Orange":0xFF7F00],
+        ["Black":0x000000],
+        ["White":0xFFFFFF],
+        ["Pink":0xFF6EB4],
+        ["Purple":0x8968CD],
+        ["Gray":0xBEBEBE],
+        ["Yellow":0xFFFF00]
+    ]
+    
+    init(){
+        
+    }
+}
+
