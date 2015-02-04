db1528359c72cfc26fb1c668659582bff427ce64
rugbyprof <terry.griffin@mwsu.edu>
2015-02-04 15:56:22 -0600

    finishing treasure example

--- TreasureHunt-Starter/TreasureHunt/GeoLocation.swift
+++ TreasureHunt-Starter/TreasureHunt/GeoLocation.swift
@@ -7,8 +7,32 @@
 //
 
 import Foundation
+import MapKit
 
 struct GeoLocation {
     var latitude: Double
     var longitude: Double
+    
+    func distanceBetween(other: GeoLocation) -> Double {
+        let locationA = CLLocation(latitude: self.latitude,longitude: self.longitude)
+        let locationB = CLLocation(latitude: other.latitude,longitude: other.longitude)
+        return locationA.distanceFromLocation(locationB)
+    }
+}
+
+extension GeoLocation {
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2DMake(self.latitude, self.longitude)
+    }
+    var mapPoint: MKMapPoint {
+        return MKMapPointForCoordinate(self.coordinate)
+    }
+}
+
+extension GeoLocation: Equatable {
+}
+
+func ==(lhs: GeoLocation, rhs: GeoLocation) -> Bool {
+    return lhs.latitude == rhs.latitude &&
+    lhs.longitude == rhs.longitude
 }
--- TreasureHunt-Starter/TreasureHunt/Treasure.swift
+++ TreasureHunt-Starter/TreasureHunt/Treasure.swift
@@ -7,8 +7,13 @@
 //
 
 import Foundation
+import MapKit
 
-class Treasure {
+@objc protocol Alertable {
+    func alert() -> UIAlertController
+}
+
+class Treasure: NSObject {
     let what: String
     let location: GeoLocation
     
@@ -24,38 +29,85 @@ class Treasure {
         )
         self.init(what: what, location: location)
     }
+    
+    func pinColor() -> MKPinAnnotationColor {
+        return MKPinAnnotationColor.Red
+    }
+}
+
+extension Treasure: MKAnnotation {
+    var coordinate: CLLocationCoordinate2D {
+        return self.location.coordinate
+    }
+    var title: String {
+        return self.what
+    }
 }
 
 
-class HistoryTreasure: Treasure {
+final class HistoryTreasure: Treasure {
         
-        let year: Int
-        
-        init(what: String, year: Int,latitude: Double, longitude: Double){
-            self.year = year
-            let location = GeoLocation(latitude: latitude,longitude: longitude)
-            super.init(what: what, location: location)
-        }
+    let year: Int
+    
+    init(what: String, year: Int,latitude: Double, longitude: Double){
+        self.year = year
+        let location = GeoLocation(latitude: latitude,longitude: longitude)
+        super.init(what: what, location: location)
+    }
+    
+    override func pinColor() -> MKPinAnnotationColor {
+        return MKPinAnnotationColor.Purple
+    }
 }
 
-class FactTreasure: Treasure {
+final class FactTreasure: Treasure {
         
-        let fact: String
-        
-        init(what: String, fact: String,latitude: Double, longitude: Double){
-            self.fact = fact
-            let location = GeoLocation(latitude: latitude,longitude: longitude)
-            super.init(what: what, location: location)
-        }
+    let fact: String
+    
+    init(what: String, fact: String,latitude: Double, longitude: Double){
+        self.fact = fact
+        let location = GeoLocation(latitude: latitude,longitude: longitude)
+        super.init(what: what, location: location)
+    }
+                
 }
 
-class HQTreasure: Treasure {
+final class HQTreasure: Treasure {
             
-        let company: String
-        
-        init(company: String, latitude: Double, longitude: Double) {
-            self.company = company
-            let location = GeoLocation(latitude: latitude,longitude: longitude)
-            super.init(what: company + " headquarters",location: location)
-        }
+    let company: String
+    
+    init(company: String, latitude: Double, longitude: Double) {
+        self.company = company
+        let location = GeoLocation(latitude: latitude,longitude: longitude)
+        super.init(what: company + " headquarters",location: location)
+    }
+            
+    override func pinColor() -> MKPinAnnotationColor {
+        return MKPinAnnotationColor.Green
+    }
+}
+
+extension HistoryTreasure: Alertable {
+    func alert() -> UIAlertController {
+        let alert = UIAlertController(title: "History",
+                                      message: "From \(self.year):\n\(self.what)",
+                                      preferredStyle: UIAlertControllerStyle.Alert)
+        return alert
+    }
+}
+extension FactTreasure: Alertable {
+    func alert() -> UIAlertController {
+        let alert = UIAlertController(title: "Fact",
+                                      message: "From \(self.what):\n\(self.fact)",
+                                      preferredStyle: UIAlertControllerStyle.Alert)
+        return alert
+    }
+}
+extension HQTreasure: Alertable {
+    func alert() -> UIAlertController {
+        let alert = UIAlertController(title: "Headquarters",
+                                      message: "The headquarters of \(self.company)",
+                                      preferredStyle: UIAlertControllerStyle.Alert)
+        return alert
+    }
 }
