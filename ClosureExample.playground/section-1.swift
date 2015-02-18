e27123d709c687ea02f71522162345fe05e2236f
rugbyprof <terry.griffin@mwsu.edu>
2015-02-18 10:14:12 -0600

    don't know what im adding

--- ClosureExample.playground/section-1.swift
+++ ClosureExample.playground/section-1.swift
@@ -12,6 +12,14 @@ func cube(a:Float) -> Float {
     return a * a * a
 }
 
+func notfloor(a:Float) -> Int {
+    return Int(a)
+}
+
+func lunges(tights:Int) -> Int{
+    return tights
+}
+
 func averageSumOfSquares(a:Float,b:Float) -> Float {
     return (square(a) + square(b)) / 2.0
 }
@@ -29,9 +37,13 @@ func averageOfFunction(a:Float,b:Float,f:(Float -> Float)) -> Float {
     return (f(a) + f(b)) / 2
 }
 
+
+
 //Calling the function w/ closure
-averageOfFunction(3, 4, square)
-averageOfFunction(3, 4, cube)
+//averageOfFunction(3, 4, square)
+//averageOfFunction(3, 4, cube)
+//averageOfFunction(3, 4, floor)
+
 
 /*****************************************/
 //My answer
@@ -59,6 +71,9 @@ var Nums :[Int] = [3,4,5,2,2,6,7,4,8]
 
 
 averageSumOfNs(Nums,2)
+
+averageOfFunction(3, 4, {(x: Float) -> Float in return x * x})
+
 
 
 
