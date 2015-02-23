c49969be4f42290ed4cc0190f37503215af745a2
rugbyprof <terry.griffin@mwsu.edu>
2015-02-22 21:16:15 -0600

    cleaning house

--- Swift by Tutorials/Chapter 5 - Functions and Closures/ExampleEight_Closures.playground/section-1.swift
+++
@@ -1,8 +0,0 @@
-let animals = ["fish", "cat", "chicken", "dog"]
-
-var isBefore = {    (one: String, two: String) -> Bool in  return one > two}let sortedStrings = animals.sorted(isBefore)
-
-let sortedStrings2 = animals.sorted { $0 > $1 }
-
-let sortedStrings3 = animals.sorted(>)
-
--- iOS 8 by Tutorials/Swift/Chapter 25 - Intermediate WebKit/RWPedia-final/RWPedia_swift/hideBio.js
+++
@@ -1,3 +0,0 @@
-var styleNode = document.createElement("style"); 
-styleNode.textContent = '#user-details {display: none}'; 
-document.documentElement.appendChild(styleNode);
