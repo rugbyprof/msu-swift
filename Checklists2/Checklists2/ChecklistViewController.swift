ecefe8432e9f3ff4662f2ce82675ccee29fdc277
rugbyprof <terry.griffin@mwsu.edu>
2015-02-25 18:50:36 -0600

    stuff

--- Checklists2/Checklists2/ChecklistViewController.swift
+++ Checklists2/Checklists2/ChecklistViewController.swift
@@ -21,6 +21,41 @@ class ChecklistViewController: UITableViewController {
     var row2checked = false
     var row3checked = true
     var row4checked = false
+    
+    var items: [ChecklistItem]
+    
+    required init(coder aDecoder: NSCoder) {
+        
+        items = [ChecklistItem]()
+        
+        let row0item = ChecklistItem()
+        row0item.text = "Item 1"
+        row0item.checked = false
+        items.append(row0item)
+        
+        let row1item = ChecklistItem()
+        row1item.text = "Item 2"
+        row1item.checked = true
+        items.append(row1item)
+    
+        let row2item = ChecklistItem()
+        row2item.text = "Item 3"
+        row2item.checked = false
+        items.append(row2item)
+        
+        let row3item = ChecklistItem()
+        row3item.text = "Item 4"
+        row3item.checked = true
+        items.append(row3item)
+        
+        let row4item = ChecklistItem()
+        row4item.text = "Item 5"
+        row4item.checked = false
+        items.append(row4item)
+
+        super.init(coder: aDecoder)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +69,7 @@ class ChecklistViewController: UITableViewController {
 
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return items.count
     }
     
     
@@ -42,21 +77,10 @@ class ChecklistViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem")as UITableViewCell
         
-        
         let label = cell.viewWithTag(1000) as UILabel
-
-        if indexPath.row == 0 {
-            row0text = "Walk the dog"
-        } else if indexPath.row == 1 {
-            row0text = "Brush my teeth"
-        } else if indexPath.row == 2 {
-            row0text = "Learn iOS development"
-        } else if indexPath.row == 3 {
-            row0text = "Soccer practice"
-        } else if indexPath.row == 4 {
-            row0text = "Eat ice cream"
-        }
         
+        label.text = items[indexPath.row].text
+
         configureCheckmarkForCell(cell, indexPath: indexPath)
         
         return cell
@@ -64,20 +88,9 @@ class ChecklistViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
             
-            
         if let cell = tableView.cellForRowAtIndexPath(indexPath) {
         
-            if indexPath.row == 0 {
-                row0checked = !row0checked
-            } else if indexPath.row == 1 {
-                row1checked = !row1checked
-            } else if indexPath.row == 2 {
-                row2checked = !row2checked
-            } else if indexPath.row == 3 {
-                row3checked = !row3checked
-            } else if indexPath.row == 4 {
-                row4checked = !row4checked
-            }
+            items[indexPath.row].checked = !items[indexPath.row].checked
         
             configureCheckmarkForCell(cell, indexPath: indexPath)
         
@@ -87,24 +100,13 @@ class ChecklistViewController: UITableViewController {
     }
     
     func configureCheckmarkForCell(cell: UITableViewCell, indexPath: NSIndexPath) {
-        var isChecked = false
-        if indexPath.row == 0 {
-            isChecked = row0checked
-        } else if indexPath.row == 1 {
-            isChecked = row1checked
-        } else if indexPath.row == 2 {
-            isChecked = row2checked
-        } else if indexPath.row == 3 {
-            isChecked = row3checked
-        } else if indexPath.row == 4 {
-            isChecked = row4checked
-        }
-            
-        if isChecked {
+                    
+        if items[indexPath.row].checked {
             cell.accessoryType = .Checkmark
         } else {
             cell.accessoryType = .None
         }
+                    
     }
 
 }
